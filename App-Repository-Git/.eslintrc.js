256339e604d2bfd6bb5e6b52d16494b4012b8ade
uillian <uillianhenrique@hotmail.com>
2020-07-28 01:55:28 -0300

    add reactotron react-native

--- App-Repository-Git/.eslintrc.js
+++ App-Repository-Git/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        es2020: true,
+        es6: true,
     },
     extends: [
         'plugin:react/recommended',
@@ -8,12 +8,17 @@ module.exports = {
         'prettier',
         'prettier/react',
     ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+        __DEV__: 'readonly',
+    },
     parser: 'babel-eslint',
     parserOptions: {
         ecmaFeatures: {
             jsx: true,
         },
-        ecmaVersion: 11,
+        ecmaVersion: 2018,
         sourceType: 'module',
     },
     plugins: ['react', 'prettier'],
