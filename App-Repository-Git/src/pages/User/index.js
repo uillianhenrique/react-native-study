e3f831e07fe7a2da83873b135b393400d076cc60
uillian <uillianhenrique@hotmail.com>
2020-07-29 01:38:25 -0300

    styling the components

--- App-Repository-Git/src/pages/User/index.js
+++ App-Repository-Git/src/pages/User/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 const User = () => {
-    return <Container />;
+    return (
+        <Container>
+            <Form>
+                <Input
+                    autoCorret={false}
+                    autoCapitalize="none"
+                    placeholder="Adicionar usuário"
+                />
+                <SubmitButton>
+                    <Icon name="add" size={20} color="#fff" />
+                </SubmitButton>
+            </Form>
+        </Container>
+    );
 };
 
 export default User;
